9f799e31c406992efab86d2fe010b65f3cf0a92d
M HARIKA <harika111205@gmail.com>
2026-08-04 12:40:06 +0530

    updated experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Hackathon Participant',
+    company: "IEEE Computer Society BITM.",
+    duration: "December 2025"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Hackathon Participant",
+    company: "Future Tech Summit - Hackathon Challenge",
+    duration: "December 2025"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Hackathon Participant",
+    company: "Logitech and Aspire for Her",
+    duration: "July 2026"
   }
 ]
