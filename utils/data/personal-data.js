2c2d1aee26a7014f87dae2c3915890b8054dc114
M HARIKA <harika111205@gmail.com>
2026-08-05 07:24:08 +0530

    resume path updation

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,12 +4,12 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is M HARIKA . I'm a Computer Science Engineering student passionate about Artificial Intelligence, Machine Learning, and Backend Development. I enjoy building practical web applications and AI-powered solutions while continuously learning new technologies.My skills include Python, FastAPI, TensorFlow, NumPy, React, HTML, CSS, DBMS, and Data Structures & Algorithms. I love solving real-world problems through code and am always looking for opportunities to learn, collaborate, and grow as a software developer.I'm currently expanding my knowledge in Generative AI and modern backend technologies while working on projects that strengthen my development and problem-solving skills.",
   email: 'harikam500@gmail.com',
-  phone: '+91 6366109664',
-  address: 'New Indra Nagar ,SRP Colony 16th ward 2nd cross house no 166 near new auto stand Ballari Karnataka 583101',
+  phone: '+916366109664',
+  address: 'New Indra Nagar ,SRP Colony ,16th ward 2nd cross ,house no 166 ,near new auto stand Ballari,Karnataka 583101',
   github: 'https://github.com/Hariiika-srinivas',
   instagram: 'https://www.instagram.com/harika._srinivas._/',
   linkedIn: 'www.linkedin.com/in/m-harika-b9753a397',
   leetcode: "https://leetcode.com/u/Mharika05/",
   devUsername: "Mharika-dev",
-  resume: "c:\Users\Harika123\Downloads\updated resume .pdf"
+  resume: "/resume.pdf"
 }
